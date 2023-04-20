78c1d1de68396d66d201ad783d8cfb6a3addbed0
Henri <henri@fixmycity.de>
2023-04-20 22:50:17 +0200

    Add support for mailjet mails

---
+++ config/plugins.js
@@ -0,0 +1,17 @@
+module.exports = ({ env }) => ({
+  email: {
+    config: {
+      provider: "strapi-provider-email-mailjet",
+        providerOptions: {
+          publicApiKey: env("MAILJET_PUBLIC_KEY"),
+          secretApiKey: env("MAILJET_SECRET_KEY"),
+        },
+        settings: {
+          defaultFrom: "no-reply@plan-f.info",
+          defaultFromName: "Plan F Team",
+          defaultTo: "info@fixmycity.de",
+          defaultToName: "FixMyCity",
+        },
+      },
+    }
+});
