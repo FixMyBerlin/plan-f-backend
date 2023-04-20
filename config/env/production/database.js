4af95274ad7b65abb798111ab1bedff8c1f2049f
Henri <henri@fixmycity.de>
2023-04-20 23:24:36 +0200

    Fix username env var in database.js

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => ({
       host: env('DATABASE_HOST', 'localhost'),
       port: env.int('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME'),
-      user: env('DATABASE_USERNAME'),
+      user: env('DATABASE_USER'),
       password: env('DATABASE_PASSWORD'),
     },
     debug: false,
