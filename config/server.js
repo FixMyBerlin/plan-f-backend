e69ee9004892ef2c34bada78b6676e25727acd03
Henri <henri@fixmycity.de>
2023-05-16 14:14:27 +0200

    Update url to new backend domain

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'https://plan-f.tiummk647p9vk.eu-central-1.cs.amazonlightsail.com/',
+  url: 'https://backend.plan-f.info/',
   app: {
     keys: env.array('APP_KEYS'),
   },
